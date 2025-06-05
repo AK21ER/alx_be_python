8dfd845f0678cbc9d1f752ede18f47ce4d37d154
AK21ER <itsmereka21@gmail.com>
2025-06-05 13:23:25 -0700

    Update basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -1,8 +1,11 @@
 number1 = 10
 number2 = 5
-sum =number1+number2
-difference=number2-number1
-product=number1*number2
-print("Addition of " + str(number1) +" "+ "and"+ " "  + str(number2) + " " + "is" + " " + str(sum))
-print("Subtraction of " + str(number2) + " " + "and"  + " " + str(number1)    + " " + "is" + " " + str(difference))
-print("Multiplication of " + str(number1) + " " + "and" + " "  + str(number2) +  " " +"is" + " " + str(product)) 
+
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
+
