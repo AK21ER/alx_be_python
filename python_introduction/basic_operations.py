b3fb3615f6db882130276ce77823e940053d40db
AK21ER <itsmereka21@gmail.com>
2025-06-05 23:09:11 -0700

    add

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1=5
+number2=10
+sum =number1+number2
+difference=number2-number1
+product=number1*number2
+print("Addition of " + str(number1) +" "+ "and"+ " "  + str(number2) + " " + "is" + " " + str(sum))
+print("Subtraction of " + str(number2) + " " + "and"  + " " + str(number1)    + " " + "is" + " " + str(difference))
+print("Multiplication of " + str(number1) + " " + "and" + " "  + str(number2) +  " " +"is" + " " + str(product)) 
